03a9e527477438c0e7c1c3411649a45f232f7321
JohnrBell <johnrbell@gmail.com>
2015-02-17 20:18:18 -0500

    modded seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,4 @@ User.create(name: 'Josh', password:'test')
 Link.create(local: 'link1', external:'http://www.reddit.com/r/battlestations')
 Link.create(local: 'link2', external:'http://www.reddit.com/r/everydaycarry')
 Link.create(local: 'link3', external:'http://www.reddit.com/r/space')
-Link.create(local: 'test/link1', external:'http://www.reddit.com/r/funny')Link.create!([
-  {local: "wtf", external: "http://www.reddit.com/r/wtf"},
-  {local: "desgin_comps-client2", external: "http://docs.google.com/"},
-  {local: "desgin_comps-link1", external: "http://www.reddit.com/r/battlestations"},
-  {local: "desgin_comps-link2", external: "http://www.reddit.com/r/everydaycarry"},
-  {local: "desgin_comps-link3", external: "http://www.reddit.com/r/space"},
-  {local: "shit", external: "http://www.reddit.com/r/funny"}
-])
+Link.create(local: 'test/link1', external:'http://www.reddit.com/r/funny')
